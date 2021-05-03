c6866bcba12ae3d04b6db1c584488785276360cd
4Ykw <6281545+4Ykw@users.noreply.github.com>
2021-05-03 07:52:18 +0000

    Fixing the log for witnessing messages

--- he_awm.sh
+++ he_awm.sh
@@ -41,16 +41,21 @@ NODE_DOWN=`pm2 status | grep ${PM2_NODE_NAME} | grep stopped|wc -l`
 # (FIX) - Play with arrays and reduce the amount of queries to IO
 
 # Find current <Round>
+# Adjust number of lines depending on log history messages
 CURRENT_ROUND=`tail -n 1000 ${NODE_APP_LOG} | grep P2P | grep currentRound | tail -n 1 | cut -d " " -f 6`
 
 # Find any recent <Round> witness messages
-# (TODO) - use another method as tail generates repeated messages (adjust tail lines for repetition adjustment)
-WITNESS_INFO[0]=`tail -n 1500 ${NODE_APP_LOG} | grep Blockchain | grep ${WITNESS_NAME} | grep ${CURRENT_ROUND} | grep scheduled | tail -n 1`
-WITNESS_INFO[1]=`tail -n 500 ${NODE_APP_LOG} | grep Blockchain | grep ${WITNESS_NAME} | grep ${CURRENT_ROUND} | grep signed | tail -n 1`
+# (TODO) - use another method as tail generates repeated messages (adjust tail lines for log history adjustment)
+WITNESS_INFO[0]=`tail -n 1000 ${NODE_APP_LOG} | grep Blockchain | grep ${WITNESS_NAME} | grep ${CURRENT_ROUND} | grep scheduled | tail -n 1 | awk '{ print $5, $8, $9, $10, $11, $12" - Last log message ("$1 , $2")" }'`
+WITNESS_INFO[1]=`tail -n 1000 ${NODE_APP_LOG} | grep Blockchain | grep ${WITNESS_NAME} | grep ${CURRENT_ROUND} | grep signed | tail -n 1 | awk '{ print $7, $8, $9" - Last log message ("$1 , $2")" }'`
 # Print if there is any message
 if [ -n "${WITNESS_INFO[0]}" ]; then
-	echo ${WITNESS_INFO[@]}
+	echo $(timestamp_format)${WITNESS_INFO[0]}
 fi
+if [ -n "${WITNESS_INFO[1]}" ]; then
+        echo $(timestamp_format)${WITNESS_INFO[1]}
+fi
+
 
 # Get number of blocks still missing from last streamer message
 BLOCKS_MISSING=`tail -n 333 ${NODE_APP_LOG} | grep Streamer | grep head_block_number | tail -n 1 | cut -d" " -f 12`
@@ -70,14 +75,14 @@ elif [ "${NODE_DOWN}" == "0" ] && [ "${BLOCKS_MISSING}" != "" ]; then
 	# The order of the IFs matter!
         if [ "${TIMES_MISSING}" == "0" ] && [ "${BLOCKS_MISSING}" == "0" ]; then
                 # No missed blocks, therefore we can be sure to continue signing or register
-                echo $(timestamp_format)"Witness State[${SIGNING_BLOCKS}] - Node is stable and in sync!"
+                echo $(timestamp_format)"Witness State[${SIGNING_BLOCKS}] Round[${CURRENT_ROUND}] - Node is stable and in sync!"
                 REGISTER="1"
 	elif [[ ( "${TIMES_MISSING}" == "1" ) || ( "${TIMES_MISSING}" == "2" ) ]] && [[ ( "${BLOCKS_MISSING}" == "1" ) || ( "${BLOCKS_MISSING}" == "0" ) ]]; then
 		# Let's assume that one or two times behind and up to 1 block missed is an evaluation zone A (no decisions)
-		echo $(timestamp_format)"Witness State[${SIGNING_BLOCKS}] - Evaluation threshold... [${BLOCKS_MISSING}]BM [${TIMES_MISSING}]TM"
+		echo $(timestamp_format)"Witness State[${SIGNING_BLOCKS}] Round[${CURRENT_ROUND}] - Evaluation threshold... [${BLOCKS_MISSING}]BM [${TIMES_MISSING}]TM"
 	else
                 # If there are more than one message with missing blocks and still out of sync, then indicate to unregister
-                echo $(timestamp_format)"Witness State[${SIGNING_BLOCKS}] - Node is unstable with: [${BLOCKS_MISSING}]BM [${TIMES_MISSING}]TM"
+                echo $(timestamp_format)"Witness State[${SIGNING_BLOCKS}] Round[${CURRENT_ROUND}] - Node is unstable with: [${BLOCKS_MISSING}]BM [${TIMES_MISSING}]TM"
                 REGISTER="0"
 	fi
 else
